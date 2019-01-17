9bbf5ea5b087f451df9be81244d8c20693c0242e
Arafat <arafat.ict.mbstu@gmail.com>
2019-01-17 10:00:18 +0600

    checking

--- app/src/main/java/com/arafat/retrosample/network/ApiInterface.java
+++ app/src/main/java/com/arafat/retrosample/network/ApiInterface.java
@@ -2,9 +2,13 @@ package com.arafat.retrosample.network;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiInterface {
 
-    @GET("/json")
-    Call<ServerResponse> getMyIp();
+   /* @GET("/json")
+    Call<ServerResponse> getMyIp();*/
+
+    @POST("/json")
+    Call<ServerResponse>getMyIp();
 }
